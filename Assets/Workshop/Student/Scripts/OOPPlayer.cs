4451bb9a3beb467585be20e71a23e6f4dbd16b42
novadesu-cm <kimkup53@gmail.com>
2026-09-15 13:54:25 +0700

    งานในคลาส

--- Assets/Workshop/Student/Scripts/OOPPlayer.cs
+++ Assets/Workshop/Student/Scripts/OOPPlayer.cs
@@ -7,7 +7,6 @@ using UnityEngine.UIElements;
 
 namespace Solution
 {
-
     public class OOPPlayer : Character
     {
         public Inventory inventory;
@@ -43,9 +42,20 @@ namespace Solution
 
         public void UseFireStorm()
         {
-            if (inventory.HasItem("FireStorm",1))
+            if (inventory.HasItem("FireStorm", 1))
             {
-                //stundent exercise: use FireStorm to attack 3 lower energy enemies on map
+                
+                inventory.UseItem("FireStorm", 1);
+                OOPEnemy[] enemies = SortEnemiesByRemainningEnergy1();
+                int count = 3;
+                if (count > enemies.Length)
+                {
+                    count = enemies.Length;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    enemies[i].TakeDamage(10);
+                }
             }
             else
             {
@@ -55,7 +65,20 @@ namespace Solution
         public OOPEnemy[] SortEnemiesByRemainningEnergy1()
         {
             var enemies = mapGenerator.GetEnemies();
-            //stundent exercise: sort enemies by remainning energy
+
+            
+            for (int i = 0; i < enemies.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < enemies.Length; j++)
+                {
+                    if (enemies[j].energy < enemies[minIndex].energy)
+                    {
+                        minIndex = j;
+                    }
+                }
+                (enemies[i], enemies[minIndex]) = (enemies[minIndex], enemies[i]);
+            }
 
             return enemies;
         }
@@ -63,7 +86,9 @@ namespace Solution
         public OOPEnemy[] SortEnemiesByRemainningEnergy2()
         {
             var enemies = mapGenerator.GetEnemies();
-            //stundent exercise: sort enemies by remainning energy
+
+          
+            Array.Sort(enemies, (a, b) => a.energy.CompareTo(b.energy));
 
             return enemies;
         }
@@ -80,7 +105,5 @@ namespace Solution
                 Debug.Log("Player is Dead");
             }
         }
-
     }
-
 }
