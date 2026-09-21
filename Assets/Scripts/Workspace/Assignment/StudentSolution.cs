ec18bb6e9a3cc405239e67e52584677a02107d61
novadesu-cm <kimkup53@gmail.com>
2026-09-21 20:22:43 +0700

    Assignment

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -11,44 +11,37 @@ namespace Assignment
         #region Lecture
         public int[] LCT01_SelectionSortAscending(int[] numbers)
         {
-            int n = numbers.Length;
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
                 int minIndex = i;
-                for (int j = i + 1; j < n; j++)
+                for (int j = i; j < numbers.Length; j++)
                 {
                     if (numbers[j] < numbers[minIndex])
                     {
                         minIndex = j;
                     }
                 }
+
+                // int temp = numbers[minIndex];
+                // numbers[minIndex] = numbers[i];
+                // numbers[i] = temp;
+
                 (numbers[i], numbers[minIndex]) = (numbers[minIndex], numbers[i]);
-            }
-            foreach (var n_ in numbers)
-            {
-                Debug.Log(n_);
             }
             return numbers;
         }
 
         public int[] LCT02_BubbleSortAscending(int[] numbers)
         {
-            int n = numbers.Length;
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                for (int j = 0; j < n - i - 1; j++)
+                for (int j = 0; j < numbers.Length - i - 1; j++)
                 {
                     if (numbers[j] > numbers[j + 1])
                     {
-                        int temp = numbers[j];
-                        numbers[j] = numbers[j + 1];
-                        numbers[j + 1] = temp;
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
                     }
                 }
-            }
-            foreach (var n_ in numbers)
-            {
-                Debug.Log(n_);
             }
             return numbers;
         }
@@ -56,7 +49,7 @@ namespace Assignment
         public int[] LCT03_InsertionSortAscending(int[] numbers)
         {
             int n = numbers.Length;
-            for (int i = 0; i < n; i++)
+            for (int i = 1; i < n; i++)
             {
                 int key = numbers[i];
                 int j = i - 1;
@@ -67,10 +60,6 @@ namespace Assignment
                 }
                 numbers[j + 1] = key;
             }
-            foreach (var n_ in numbers)
-            {
-                Debug.Log(n_);
-            }
             return numbers;
         }
 
@@ -80,22 +69,73 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+                (numbers[i], numbers[maxIndex]) = (numbers[maxIndex], numbers[i]);
+            }
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] < key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
-            return 0;
+            if (numbers == null || numbers.Length < 2) return 0;
+
+            int largest = int.MinValue;
+            int secondLargest = int.MinValue;
+
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                {
+                    secondLargest = largest;
+                    largest = num;
+                }
+                else if (num > secondLargest && num != largest)
+                {
+                    secondLargest = num;
+                }
+            }
+            return secondLargest == int.MinValue ? largest : secondLargest;
         }
 
         #endregion
@@ -104,7 +144,31 @@ namespace Assignment
 
         public int EX01_FindLongestConsecutiveSequence(int[] numbers)
         {
-            return 0;
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            HashSet<int> numSet = new HashSet<int>(numbers);
+            int longestStreak = 0;
+
+            foreach (int num in numSet)
+            {
+                if (!numSet.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int currentStreak = 1;
+
+                    while (numSet.Contains(currentNum + 1))
+                    {
+                        currentNum += 1;
+                        currentStreak += 1;
+                    }
+
+                    if (currentStreak > longestStreak)
+                    {
+                        longestStreak = currentStreak;
+                    }
+                }
+            }
+            return longestStreak;
         }
 
         #endregion
